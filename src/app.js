7637e7a3952721a6cb9bebad6839daef4b350001
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-01-13 17:01:19 +0000

    exersice in progress

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ window.onload = function() {
   const form = document.querySelector("#payment-form");
   const submitButton = document.querySelector("#submit-button");
   let formData = {
-    cnn: "",
+    ccn: "",
     cvc: "",
     amount: "",
     firstName: "",
@@ -21,5 +21,50 @@ window.onload = function() {
   form.addEventListener("change", e => {
     formData[e.target.name] = e.target.value;
     console.log(formData);
+  });
+
+  submitButton.addEventListener("click", e => {
+    e.preventDefault();
+    if (formData.ccn.length > 0) {
+      const ccnInput = document.querySelector("#ccn-input");
+      if (isNaN(formData.ccn) === false) {
+        console.log("Yay it's a number!");
+        if (formData.ccn.length === 16) {
+          ccnInput.classList.add("green-border");
+          ccnInput.classList.remove("red");
+        } else {
+          ccnInput.value = "";
+          ccnInput.placeholder = "Card # must be 16 numbers long!";
+          ccnInput.classList.add("red");
+        }
+      } else {
+        console.log("Not a number :(");
+      }
+    } else {
+      const ccnInput = document.querySelector("#ccn-input");
+      ccnInput.placeholder = "This field is empty!";
+      ccnInput.classList.add("red");
+    }
+
+    if (formData.cvc.length > 0) {
+      const cvcInput = document.querySelector("#cvc-input");
+      if (isNaN(formData.ccn) === false) {
+        console.log("Yay it's a number!");
+        if (formData.ccn.length === 3) {
+          cvcInput.classList.add("green-border");
+          cvcInput.classList.remove("red");
+        } else {
+          cvcInput.value = "";
+          cvcInput.placeholder = "CVC must be 3 numbers long!";
+          cvcInput.classList.add("red");
+        }
+      } else {
+        console.log("Not a number :(");
+      }
+    } else {
+      const cvcInput = document.querySelector("#cvc-input");
+      cvcInput.placeholder = "This field is empty!";
+      cvcInput.classList.add("red");
+    }
   });
 };
