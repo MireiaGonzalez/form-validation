3f0a51719e4119d048cdcdae3d2ea72829d5804d
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-01-13 16:16:14 +0000

    exersice in progress

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ import "./style.css";
 
 window.onload = function() {
   const form = document.querySelector("#payment-form");
-  let formData = {};
+  const submitButton = document.querySelector("#submit-button");
+  let formData = {
+    cnn: "",
+    cvc: "",
+    amount: "",
+    firstName: "",
+    lastName: "",
+    city: "",
+    state: "",
+    postalCode: "",
+    paymentMethod: "",
+    notes: ""
+  };
 
   form.addEventListener("change", e => {
     formData[e.target.name] = e.target.value;
